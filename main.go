c10e6728cdc6e0e20f1a0a4da563053c4ca5e442
Turmunkh <turuu0710@gmail.com>
2023-12-26 07:42:48 +0800

    second commit

--- main.go
+++ main.go
@@ -1,3 +1,50 @@
-import main()
+package main
 
+import (
+	"context"
+	"log"
+	"time"
 
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoInstance struct {
+	Client
+	Db
+}
+
+var mg MongoInstance
+
+const dbName = "fiber-hrms"
+const mongoURI = "mongodb://localhost:27017" + dbName
+
+type Employee struct {
+	ID     string
+	Name   string
+	Salary string
+	Age    string
+}
+
+func Connect() error {
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	context.WithTimeout(context.Background(), 30*time.Second)
+}
+
+func main() {
+
+	if err := Connect(): err != nil{
+		log.Fatal(err)
+	}
+	app := fiber.New()
+
+	app.Get("/employee", func(c *fuber.Ctx)error{
+
+	})
+	app.Post("/employee")
+	app.Put("/employee/:id")
+	app.Delete("/employee/:id")
+}
